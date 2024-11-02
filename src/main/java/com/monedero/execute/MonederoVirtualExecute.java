89adedfb34d16ba3cc2712a4b1b745c0b848d1a8
Emilio Armas <emilio.armas@epn.edu.ec>
2024-11-02 12:18:13 -0500

    update execute class

--- src/main/java/com/monedero/execute/MonederoVirtualExecute.java
+++ src/main/java/com/monedero/execute/MonederoVirtualExecute.java
@@ -3,5 +3,6 @@ package com.monedero.execute;
 public class MonederoVirtualExecute {
     public static void main(String[] args) {
         System.out.println("Monedero Virtual Execute...");
+        System.out.println("Changed...");
     }
 }
