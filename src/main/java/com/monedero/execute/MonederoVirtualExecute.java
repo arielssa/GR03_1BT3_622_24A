1d4c0e717b36ec7c4d6edb7742cd5cd45b0b8473
Emilio Armas <emilio.armas@epn.edu.ec>
2024-11-02 12:09:49 -0500

    change to derby db and create execute class

---
+++ src/main/java/com/monedero/execute/MonederoVirtualExecute.java
@@ -0,0 +1,7 @@
+package com.monedero.execute;
+
+public class MonederoVirtualExecute {
+    public static void main(String[] args) {
+        System.out.println("Monedero Virtual Execute...");
+    }
+}
