fd605a41bf4d05eff9ca204f3886b228a61b2b2d
Emilio Armas <emilio.armas@epn.edu.ec>
2024-11-03 17:47:13 -0500

    ic1 update WAR file and MonederoExecute

--- src/main/java/com/monedero/execute/MonederoVirtualExecute.java
+++ src/main/java/com/monedero/execute/MonederoVirtualExecute.java
@@ -3,6 +3,6 @@ package com.monedero.execute;
 public class MonederoVirtualExecute {
     public static void main(String[] args) {
         System.out.println("Monedero Virtual Execute...");
-        System.out.println("Prueba 2, Monedero Virtual Execute...");
+        System.out.println("Integración 1...");
     }
 }
