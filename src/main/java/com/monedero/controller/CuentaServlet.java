784d8aebb0882e55c7819b202944c3c91bdfe89d
Emilio Armas <emilio.armas@epn.edu.ec>
2024-11-03 17:46:53 -0500

    ic1 update CuentaServlet

--- src/main/java/com/monedero/controller/CuentaServlet.java
+++ src/main/java/com/monedero/controller/CuentaServlet.java
@@ -1,7 +1,10 @@
 package com.monedero.controller;
 
 import com.monedero.dao.CuentaDAO;
+import com.monedero.dao.EtiquetaDAO;
+import com.monedero.dao.UsuarioDAO;
 import com.monedero.model.Cuenta;
+import com.monedero.model.Etiqueta;
 import com.monedero.model.Usuario;
 
 import javax.servlet.ServletException;
@@ -14,29 +17,46 @@ import java.io.IOException;
 @WebServlet("/cuenta")
 public class CuentaServlet extends HttpServlet {
     private CuentaDAO cuentaDAO;
+    private EtiquetaDAO etiquetaDAO;
 
     @Override
     public void init() {
         cuentaDAO = new CuentaDAO();
+        etiquetaDAO = new EtiquetaDAO();
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String action = request.getParameter("action");
 
-        if ("create".equals(action)) {
+        if ("createAccount".equals(action)) {
             Usuario usuario = (Usuario) request.getSession().getAttribute("usuario");
             String nombre = request.getParameter("nombre");
             String numeroCuenta = request.getParameter("numeroCuenta");
             double balance = Double.parseDouble(request.getParameter("balance"));
 
-            Cuenta cuenta = new Cuenta();
-            cuenta.setNombre(nombre);
-            cuenta.setNumeroCuenta(numeroCuenta);
-            cuenta.setBalance(balance);
-            cuenta.setUsuario(usuario);
+            Cuenta cuenta = new Cuenta(nombre, numeroCuenta, usuario, balance);
 
             cuentaDAO.save(cuenta);
+
+            response.sendRedirect("cuenta");
+        }
+
+        if("actualizarBalanceLimite".equals(action)) {
+
+            int cuentaId = Integer.parseInt(request.getParameter("cuentaId"));
+            Cuenta cuenta = cuentaDAO.findById(cuentaId);
+            cuentaDAO.update(cuenta);
+            response.sendRedirect("cuenta");
+        }
+
+        if ("createTag".equals(action)) {
+            Usuario usuario = (Usuario) request.getSession().getAttribute("usuario");
+            String nombre = request.getParameter("nombre");
+
+            Etiqueta etiqueta = new Etiqueta(nombre, usuario);
+
+            etiquetaDAO.save(etiqueta);
 
             response.sendRedirect("cuenta");
         }
@@ -46,6 +66,7 @@ public class CuentaServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         Usuario usuario = (Usuario) request.getSession().getAttribute("usuario");
         request.setAttribute("cuentas", cuentaDAO.findByUsuario(usuario));
+        // request.setAttribute("etiquetas", etiquetaDAO.findByUsuario(usuario));
         request.getRequestDispatcher("cuenta.jsp").forward(request, response);
     }
 }
